2a3d70cb263c66d521441fcd3a6f3db628d440b9
Hiram <hrm.mchll@gmail.com>
2025-03-20 20:10:37 -0600

    :sparkles: mi primer código

---
+++ 01.Estructuras_de_Control/main.js
@@ -0,0 +1,15 @@
+// trabajar toda nuestra lógica
+
+//
+//
+
+/// 1. Declarar una variable
+let age = 20;
+
+// implemento una condicional/estructura de control/ if ... else
+if (age >= 18) {
+    console.log("Eres mayor de edad");
+}
+else {
+    console.log("Eres menor de edad");
+}
